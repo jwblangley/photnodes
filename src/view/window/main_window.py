f4f78434b98968878658eca20d05447f85e9fb7d
James Langley <jwblangley@gmail.com>
2021-07-16 23:28:01 +0100

    Add image canvases to main window

--- src/view/window/main_window.py
+++ src/view/window/main_window.py
@@ -1,10 +1,15 @@
 from PySide6 import QtWidgets
+from view.window.image_canvas import ImageCanvas
 
 from view.window.node_canvas import NodeCanvas
+
+CANVAS_SIZE = (500, 500)
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("Photnodes")
 
         self.widget = QtWidgets.QWidget()
         self.setCentralWidget(self.widget)
@@ -14,6 +19,16 @@ class Window(QtWidgets.QMainWindow):
         self.widget.setLayout(self.layout)
 
         self.node_canvas = NodeCanvas()
-        self.layout.addWidget(self.node_canvas,0,1)
+        self.layout.addWidget(self.node_canvas,1,0,1,2)
 
-        self.resize(600,600)
+        self.left_image_canvas = ImageCanvas(CANVAS_SIZE)
+        self.layout.addWidget(self.left_image_canvas,0,0)
+
+        self.right_image_canvas = ImageCanvas(CANVAS_SIZE)
+        self.layout.addWidget(self.right_image_canvas,0,1)
+
+        self.showMaximized()
+
+    def closeEvent(self, event):
+        self.left_image_canvas.destroy()
+        self.right_image_canvas.destroy()

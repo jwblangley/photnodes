d588efef486e4a265fb5860455dabf519e3a8cce
James Langley <jwblangley@gmail.com>
2021-07-16 22:39:00 +0100

    Implement terminal and flow nodes

---
+++ src/view/nodes/flow_node.py
@@ -0,0 +1,24 @@
+from view.nodes.header import Header
+from view.nodes.node import BaseNode
+from view.nodes.socket import Socket
+
+
+class FlowNode(BaseNode):
+
+    def __init__(self, title, **kwargs):
+        super().__init__(**kwargs)
+
+        self.header = Header(self, title)
+        self.header.setParentItem(self)
+
+        self.primary_out_socket = Socket("_primary_in", "", True, maxConnections=1)
+        self.primary_out_socket.setParentItem(self)
+        self.primary_out_socket.node = self
+        self.sockets["_primary_in"] = self.primary_out_socket
+
+        self.primary_out_socket = Socket("_primary_out", "", False)
+        self.primary_out_socket.setParentItem(self)
+        self.primary_out_socket.node = self
+        self.sockets["_primary_out"] = self.primary_out_socket
+
+        self.updateSize()

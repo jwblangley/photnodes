bb457af31e2a74488aa7ddb3dcc9efeb78b21749
James Langley <jwblangley@gmail.com>
2021-07-19 16:08:21 +0100

    Fix caching

--- src/model/nodes/node.py
+++ src/model/nodes/node.py
@@ -17,21 +17,23 @@ class BaseNode:
             "check_required_connections should be implemented in subclasses"
         )
 
-    def is_dirty(self):
-        if self._dirty:
-            return True
+    def set_attribute(self, name, value):
+        if not (hasattr(self, name) and getattr(self, name) == value):
+            setattr(self, name, value)
+            self._dirty = True
 
+    def flag_dirty(self):
         for node in self.input_connections.values():
-            if node.is_dirty():
-                return True
+            if node.flag_dirty():
+                self._dirty = True
 
-        return False
+        return self._dirty
 
     def calculate(self, dependencies):
         raise NotImplementedError("calculate should be implemented in subclasses")
 
     def _calculate(self, dependencies):
-        if not self.is_dirty():
+        if not self._dirty:
             return self._result
 
         # Wait for dependency calculation completion
@@ -42,13 +44,17 @@ class BaseNode:
 
         return self._result
 
-    def process(self, executor):
+    def process(self, executor, check_dirty=True):
         if not self.check_required_connections():
             return executor.submit(lambda: None)
 
+        if check_dirty:
+            self.flag_dirty()
+
         # Queue dependent results
         dependencies = {
-            k: v.process(executor) for k, v in self.input_connections.items()
+            k: v.process(executor, check_dirty=False)
+            for k, v in self.input_connections.items()
         }
 
         return executor.submit(self._calculate, dependencies)
--- tests/model/nodes/node_test.py
+++ tests/model/nodes/node_test.py
@@ -126,3 +126,75 @@ def test_basic_operation_is_cached():
 
         assert n2.process(executor).result() == 3
         n2.calculate.assert_called_once()
+
+
+@pytest.mark.timeout(1)
+def test_basic_operation_is_dirty_when_attribute_changed():
+    n1 = TestStartingNodeOne()
+    n2 = TestOperationNodeAddTwo()
+
+    n2.set_input_connection("input", n1)
+    n2.calculate = Mock(return_value=3)
+
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        assert n2.process(executor).result() == 3
+        n2.calculate.assert_called_once()
+
+        n2.set_attribute("test", 1)
+
+        assert n2.process(executor).result() == 3
+        assert n2.calculate.call_count == 2
+
+
+@pytest.mark.timeout(1)
+def test_operation_two_input_paths_is_cached():
+    n1 = TestStartingNodeOne()
+    n2 = TestStartingNodeOne()
+
+    n3 = TestOperationNodeAddTwo()
+    n4 = TestOperationNodeAddTwo()
+
+    n5 = TestOperationNodeAdd()
+
+    n3.set_input_connection("input", n1)
+    n4.set_input_connection("input", n2)
+
+    n5.set_input_connection("input1", n3)
+    n5.set_input_connection("input2", n4)
+
+    n5.calculate = Mock(return_value=6)
+
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        assert n5.process(executor).result() == 6
+        n5.calculate.assert_called_once()
+
+        assert n5.process(executor).result() == 6
+        n5.calculate.assert_called_once()
+
+
+# @pytest.mark.timeout(1)
+def test_operation_two_input_paths_is_dirty_when_attribute_changed_early():
+    n1 = TestStartingNodeOne()
+    n2 = TestStartingNodeOne()
+
+    n3 = TestOperationNodeAddTwo()
+    n4 = TestOperationNodeAddTwo()
+
+    n5 = TestOperationNodeAdd()
+
+    n3.set_input_connection("input", n1)
+    n4.set_input_connection("input", n2)
+
+    n5.set_input_connection("input1", n3)
+    n5.set_input_connection("input2", n4)
+
+    n5.calculate = Mock(return_value=6)
+
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        assert n5.process(executor).result() == 6
+        n5.calculate.assert_called_once()
+
+        n1.set_attribute("test", 1)
+
+        assert n5.process(executor).result() == 6
+        assert n5.calculate.call_count == 2

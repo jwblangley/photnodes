6c1760cbb9482ff2a24a905a1c067558f865cd14
James Langley <jwblangley@gmail.com>
2021-08-17 16:31:11 +0100

    Use qt-material

--- src/main.py
+++ src/main.py
@@ -3,14 +3,19 @@ import sys
 import torch
 
 from PySide6 import QtWidgets
+
+from qt_material import apply_stylesheet
 from controller.controller import Controller
 
 from view.window.main_window import Window
+
+THEME = "dark_teal.xml"
 
 if __name__ == "__main__":
     torch.autograd.set_grad_enabled(False)
 
     app = QtWidgets.QApplication(sys.argv)
+    apply_stylesheet(app, theme=THEME)
 
     window = Window()
     controller = Controller(window)

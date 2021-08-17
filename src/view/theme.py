4040f667834528b53702ec115f7dd496a0589e81
James Langley <jwblangley@gmail.com>
2021-08-17 17:13:47 +0100

    Create theme palette
    Inherit from qt-material

--- src/view/theme.py
+++ src/view/theme.py
@@ -1,4 +1,4 @@
-import qt_material
+import os
 
 
 SPACING = 10
@@ -8,6 +8,18 @@ DEFAULT_QT_MATERIAL_THEME = "neutral_dark.xml"
 
 class _Theme:
     qt_material_theme = DEFAULT_QT_MATERIAL_THEME
+
+    @property
+    def palette(self):
+        return {
+            "primary": os.environ["QTMATERIAL_PRIMARYCOLOR"],
+            "primary_light": os.environ["QTMATERIAL_PRIMARYLIGHTCOLOR"],
+            "primary_text": os.environ["QTMATERIAL_PRIMARYTEXTCOLOR"],
+            "secondary": os.environ["QTMATERIAL_SECONDARYCOLOR"],
+            "secondary_light": os.environ["QTMATERIAL_SECONDARYLIGHTCOLOR"],
+            "secondary_dark": os.environ["QTMATERIAL_SECONDARYDARKCOLOR"],
+            "secondary_text": os.environ["QTMATERIAL_SECONDARYTEXTCOLOR"],
+        }
 
     def spacing(self, factor):
         return SPACING * factor
